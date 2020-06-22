712a5cb2641373809fbf11eb0d3a82a26b2d3e9e
Janik Luechinger <janik.luechinger@uzh.ch>
2020-06-23 00:03:39 +0200

    feat: extend rudimentary api for runner creation
    refs #104

--- manager/__main__.py
+++ manager/__main__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 SELECTION_QUEUE_NAME = '{pga_name_}@selection.queue'
 CROSSOVER_QUEUE_NAME = '{pga_name_}@crossover.queue'
@@ -9,32 +9,37 @@ MUTATION_QUEUE_NAME = '{pga_name_}@mutation.queue'
 mgr = Flask(__name__)
 
 
+@mgr.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @mgr.route("/status", methods=["GET"])
 def status():
     return "Status: OK"
 
 
-# @mgr.route("/pga", methods=["POST"])
-# def run_pga():
-#     """
-#     Run a new Parallel Genetic Algorithm in the cloud
-#
-#     :return:
-#     """
-#     print("creating pga")
-#     return
-#
-#
-# @mgr.route("/pga", methods=["GET"])
-# def get_all_pga():
-#     print("getting all pga")
-#     return
-#
-#
-# @mgr.route("/pga/<number:pga_id>", methods=["GET"])
-# def get_pga(pga_id):
-#     print("getting pga " + pga_id)
-#     return
+@mgr.route("/pga", methods=["POST"])
+def run_pga():
+    """
+    Run a new Parallel Genetic Algorithm in the cloud
+
+    :return:
+    """
+    print("creating pga")
+    return {id: 123}
+
+
+@mgr.route("/pga", methods=["GET"])
+def get_all_pga():
+    print("getting all pga")
+    return "getting all pga"
+
+
+@mgr.route("/pga/<int:pga_id>", methods=["GET"])
+def get_pga(pga_id):
+    print("getting pga {}".format(pga_id))
+    return "getting pga {}".format(pga_id)
 
 
 if __name__ == '__main__':
